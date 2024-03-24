a00a2501ad9b405275c105a860e1fb6dcdf87cfc
Maiconfz <maiconfz@gmail.com>
2024-03-24 05:01:40 +0000

    Criar repositórios JPA

---
+++ src/main/java/io/github/maiconfz/courseraprincipiosdedesenvolvimentoagildesoftwarefinalproject/data/repo/TrofeuRepository.java
@@ -0,0 +1,8 @@
+package io.github.maiconfz.courseraprincipiosdedesenvolvimentoagildesoftwarefinalproject.data.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.maiconfz.courseraprincipiosdedesenvolvimentoagildesoftwarefinalproject.data.model.Trofeu;
+
+public interface TrofeuRepository extends JpaRepository<Trofeu, Long> {
+}
