df8617f3bb6e055c5f0e6a5299219e10216278ca
Maiconfz <maiconfz@gmail.com>
2024-03-27 00:15:30 +0000

    Add login logic

---
+++ src/main/java/io/github/maiconfz/coursera_principios_de_desenvolvimento_agil_de_software_final_project/controller/IndexController.java
@@ -0,0 +1,16 @@
+package io.github.maiconfz.coursera_principios_de_desenvolvimento_agil_de_software_final_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
