fbc5c392d37e985da630d4f0fe2c3d833ea09b26
Maiconfz <maiconfz@gmail.com>
2024-03-29 03:15:43 +0000

    Adicionar listagem de livros na página inicial, ainda sem ações

--- src/main/java/io/github/maiconfz/coursera_principios_de_desenvolvimento_agil_de_software_final_project/controller/IndexController.java
+++ src/main/java/io/github/maiconfz/coursera_principios_de_desenvolvimento_agil_de_software_final_project/controller/IndexController.java
@@ -1,16 +1,23 @@
 package io.github.maiconfz.coursera_principios_de_desenvolvimento_agil_de_software_final_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import io.github.maiconfz.coursera_principios_de_desenvolvimento_agil_de_software_final_project.data.repo.LivroRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@AllArgsConstructor
 @Slf4j
 public class IndexController {
 
+    private final LivroRepository livroRepository;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("livros", livroRepository.findAll());
         return "index";
     }
 }
